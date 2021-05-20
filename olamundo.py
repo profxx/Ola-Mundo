2fd9b9aa858f58fd8e66fa9df6dc3477b14fa9fe
MarianeBM <84345926+MarianeBM@users.noreply.github.com>
2021-05-19 21:08:52 -0300

    Update olamundo.py
    Add mensagem

--- olamundo.py
+++ olamundo.py
@@ -2,10 +2,10 @@ mensagem = 'Olá universo!!!'
 mensagem = 'Bom dia! Tudo bem com você?'
 mensagem2 = 'Olá mundo'
 mensagem001 = 'Bem-Vindos ao arquivo da 2020115'
-
+mensagem3 = "Bem-Vindos ao nosso mundo!"
 print(mensagem)
 
 # print("Olá mundo")
 
 print(mensagem, mensagem001)
-
+print(mensagem3) 
